657810aca70ba2750de3f04d1ca978c37548558c
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-09-01 11:15:54 +0200

    Start working on skills section

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./skills.css";
+import Frontend from "./Frontend";
+import Backend from "./Backend";
+
+const Skills = () => {
+  return (
+    <section className="skills section" id="skills">
+      <h2 className="skills">Skills</h2>
+      <span className="skills">My technical level</span>
+
+      <div className="skills__container container grid">
+        <Frontend />
+        <Backend />
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
